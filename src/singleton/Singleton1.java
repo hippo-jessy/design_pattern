c4f8d1a8f2126e7b7f399a01110911660ea690ff
hippo <hippo.jessy@gmail.com>
2017-03-13 22:09:35 +0800

    update singleton

--- src/singleton/Singleton1.java
+++ src/singleton/Singleton1.java
@@ -3,13 +3,13 @@ package singleton;
 /**
  * Created by hippo on 3/13/17.
  *
- * This is a non-lazy and thread-safe implementation of singleton design pattern.
+ * This is a non-lazy initialized and thread-safe implementation of singleton design pattern.
  */
 public class Singleton1 {
-    private static final Singleton1 singleton = new Singleton1();
+    private static final Singleton1 instance = new Singleton1();
     private Singleton1(){};
 
     public static Singleton1 getInstance(){
-        return singleton;
+        return instance;
     }
 }
--- src/singleton/Singleton2.java
+++ src/singleton/Singleton2.java
@@ -3,7 +3,18 @@ package singleton;
 /**
  * Created by hippo on 3/13/17.
  *
- *
+ * This is a lazy initialized and non-thread safe implementation of singleton design pattern.
  */
+
 public class Singleton2 {
+    private static Singleton2 instance;
+
+    private Singleton2(){};
+
+    public Singleton2 getInstance(){
+        if(instance!=null) {
+            instance = new Singleton2();
+        }
+        return instance;
+    }
 }
